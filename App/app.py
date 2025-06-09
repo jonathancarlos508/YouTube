189fdb047369b3f1a139a9b86d074df8bc8c41ac
jonathancarlos508 <jonathancarlos508@gmail.com>
2025-06-09 19:12:16 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -11,10 +11,31 @@ st.sidebar.header("Season")
 select_season = st.sidebar.selectbox('Season',['2024/2025','2024/2023',2023/2022])
                                                
 # WebScraping Football Data
-
 def load_data(league,season):
-  url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
-  data = pd.read_csv(url)
-  return data
+  
+  if selected_league == 'England':
+    league = 'E0'
+    if selected_league == 'Germany':
+    league = 'D1'
+    if selected_league == 'Italy':
+    league = 'I1'
+    if selected_league == 'Spain':
+    league = 'SP1'
+    if selected_league == 'France':
+    league = 'F1'
+    
+    if selected_season == '2024/2025':
+    season = '2425'
+    if selected_season == '2023/2024':
+    season = '2324'
+    if selected_season == '2022/2023':
+    season = '2223'
+    
+    url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
+    data = pd.read_csv(url)
+    return data
 
 df = loead_data(selected_league, selected_season)
+
+st.subheader("Dataframe: "+selected_league)
+st.dataframe(df)
