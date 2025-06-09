8be59493e3da35e762e3dec0bd7340215efc9dfb
jonathancarlos508 <jonathancarlos508@gmail.com>
2025-06-09 15:28:52 -0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,3 +1,20 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+st.title("Web App Football Data")
+
+st.sidebar.header("Leagues")
+selected_league = st.sidebar.selectbox('League',['England','Germany','Italy','Spain','France'])
+
+st.sidebar.header("Season")
+select_season = st.sidebar.selectbox('Season',['2024/2025','2024/2023',2023/2022])
+                                               
+# WebScraping Football Data
+
+def load_data(league,season):
+  url = "https://www.football-data.co.uk/mmz4281/"+season+"/"+league+".csv"
+  data = pd.read_csv(url)
+  return data
+
+df = loead_data(selected_league, selected_season)
