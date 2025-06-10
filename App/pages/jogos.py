1539f2ce5520235959a56a8cabc772769297b5ec
jonathancarlos508 <jonathancarlos508@gmail.com>
2025-06-09 22:46:01 -0300

    Update jogos.py

--- App/pages/jogos.py
+++ App/pages/jogos.py
@@ -9,3 +9,12 @@ st.title("Jogos do Dia")
 dia = st.date_input(
 "Data de Análise",
   date.today())
+
+def load_data_jogos():
+  data_jogos = pd.read_csv("https://github.com/jonathancarlos508/YouTube/blob/main/Jogos_do_Dia/FootyStats/Jogos_do_Dia_FootyStats_"+str(dia)+".csv?raw=true")
+
+return data_jogos
+
+df_jogos = load_data_jogos()
+
+st.dataframe(df_jogos)
