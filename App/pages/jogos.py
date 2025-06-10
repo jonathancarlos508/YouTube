61e631cec194d527575a97cbe5f29336b5e224ff
jonathancarlos508 <jonathancarlos508@gmail.com>
2025-06-09 23:01:04 -0300

    Update jogos.py

--- App/pages/jogos.py
+++ App/pages/jogos.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from datetime import date
+import base64 
 
 st.title("Jogos do Dia")
 
@@ -17,3 +18,11 @@ def load_data_jogos(d):
 df_jogos = load_data_jogos(dia)
 
 st.dataframe(df_jogos)
+
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a hred="data:file/csv;base64,{b64}" download="Base_Dados.csv">Download CSV File</a>
+    return href
+
+    st.markdown(filedownload(df), unsafe_allow_html=True)
